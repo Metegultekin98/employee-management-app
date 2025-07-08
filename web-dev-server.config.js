7701bfcd07c8d926b09308ae49d594bc5bc8f79d
Mete Gultekin <metegultekin98@hotmail.com>
2025-07-08 23:29:15 +0300

    Project structure formed

--- web-dev-server.config.js
+++ web-dev-server.config.js
@@ -5,21 +5,44 @@
  */
 
 import {legacyPlugin} from '@web/dev-server-legacy';
+import {readFile} from 'fs/promises';
+import {fileURLToPath} from 'url';
+import {resolve} from 'path';
 
 const mode = process.env.MODE || 'dev';
 if (!['dev', 'prod'].includes(mode)) {
   throw new Error(`MODE must be "dev" or "prod", was "${mode}"`);
 }
 
+const indexHtmlPath = resolve(
+  fileURLToPath(import.meta.url),
+  '../src/index.html'
+);
+
 export default {
-  nodeResolve: {exportConditions: mode === 'dev' ? ['development'] : []},
+  nodeResolve: {
+    exportConditions: mode === 'dev' ? ['development'] : [],
+  },
   preserveSymlinks: true,
   plugins: [
     legacyPlugin({
       polyfills: {
-        // Manually imported in index.html file
         webcomponents: false,
       },
     }),
+  ],
+  middleware: [
+    async (ctx, next) => {
+      await next();
+
+      if (
+        ctx.response.status === 404 &&
+        !ctx.path.includes('.') &&
+        !ctx.path.startsWith('/__')
+      ) {
+        ctx.response.status = 200;
+        ctx.response.body = await readFile(indexHtmlPath, 'utf-8');
+      }
+    },
   ],
 };
